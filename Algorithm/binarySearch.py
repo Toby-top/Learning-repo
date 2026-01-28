ef7cf64e3524f17574a882a47b5fa81b4feb426f
Toby-top <damudongni40@gmail.com>
2026-01-28 19:12:28 +0800

    Added some comments
    Updated array length and value range for random integers. Added comments for clarity.

--- Algorithm/binarySearch.py
+++ Algorithm/binarySearch.py
@@ -1,8 +1,8 @@
 import random
 from time import time
 start_time = time()
-length =78
-Array = [random.randint(0,20045678765) for _ in range(length)]
+length =67 # Length of the array, which can be changed
+Array = [random.randint(0,2147483647) for _ in range(length)] # Create an array
 n = 1
 MaxGroup = ["" for _ in range(10)]
 MinGroup = ["" for _ in range(10)]
@@ -10,10 +10,12 @@ MinGroup = ["" for _ in range(10)]
 Max = Array[0]
 Min = Array[1]
 
+# Swap the variable when Array[0] < Array[1]
 if Array[0] < Array[1]:
     Max = Array[1]
     Min = Array[0]
 
+# Iteration/ Main body binary search
 for i in range(2, length, 2):
     n+=1
     if Array[i] > Array[i+1]:
@@ -29,12 +31,10 @@ for i in range(2, length, 2):
             Min = Array[i]
         n += 2
 
-
-
-
+# Output the result
 print(Array)
 print(Min)
 print(Max)
 print(n)
 end_time = time()
-print(f"{end_time - start_time:.4f} seconds")
+print(f"{end_time - start_time:.4f} seconds") # Measuring the program's running time

9d72b5897db37a5110fe06827661360ca04df3db
Toby-top <damudongni40@gmail.com>
2026-01-29 19:45:42 +0800

    Increase array length and adjust random range

--- Algorithm/binarySearch.py
+++ Algorithm/binarySearch.py
@@ -1,8 +1,8 @@
 import random
 from time import time
 start_time = time()
-length =67 # Length of the array, which can be changed
-Array = [random.randint(0,2147483647) for _ in range(length)] # Create an array
+length = 6767 # Length of the array, which can be changed
+Array = [random.randint(0,6767) for _ in range(length)] # Create an array
 n = 1
 MaxGroup = ["" for _ in range(10)]
 MinGroup = ["" for _ in range(10)]
@@ -16,8 +16,7 @@ if Array[0] < Array[1]:
     Min = Array[0]
 
 # Iteration/ Main body binary search
-for i in range(2, length, 2):
-    n+=1
+for i in range(2, length - 1, 2): 
     if Array[i] > Array[i+1]:
         if Array[i]>Max:
             Max = Array[i]
@@ -36,5 +35,4 @@ print(Array)
 print(Min)
 print(Max)
 print(n)
-end_time = time()
-print(f"{end_time - start_time:.4f} seconds") # Measuring the program's running time
+print(f"{time() - start_time:.4f} seconds") # Measuring the program's running time

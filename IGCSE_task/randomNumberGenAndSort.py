8f20837723f1ce3f5885692d9801d0bd0fdbb9e4
Toby-top <damudongni40@gmail.com>
2026-04-17 11:09:34 +0800

    Refactor random number generation and frequency counting logic

--- IGCSE_task/randomNumberGenAndSort.py
+++ IGCSE_task/randomNumberGenAndSort.py
@@ -8,3 +8,15 @@ CountedNumber = [[0] for _ in range(100000)]
 for i in range(100000):
     RandomNumber[i] = random.randint(1, 10)
 
+# count the frequency
+def Count(Value):
+    Frequency = 0 
+    for i in range(100000):
+        if RandomNumber[i] == Value:
+            Frequency += 1
+    return Frequency
+
+for Value in range(10):
+    CountedNumber[Value][0] = Value + 1
+    CountedNumber[Value][1] = Count(Value)
+

cadbeade7d4926f36077e4fd07cfa97735d60c47
Toby-top <damudongni40@gmail.com>
2026-04-17 11:14:13 +0800

    Implement sorting for counted frequency array

--- IGCSE_task/randomNumberGenAndSort.py
+++ IGCSE_task/randomNumberGenAndSort.py
@@ -20,3 +20,10 @@ for Value in range(10):
     CountedNumber[Value][0] = Value + 1
     CountedNumber[Value][1] = Count(Value)
 
+# sort the contents in the counted array
+for i in range(10):
+    for j in range(10 - 1 - i):
+        if CountedNumber[j][1] < CountedNumber[j + 1][1]:
+            Temp = CountedNumber[j][1]
+            CountedNumber[j][1] = CountedNumber[j + 1][1]
+            CountedNumber[j + 1][1] = Temp

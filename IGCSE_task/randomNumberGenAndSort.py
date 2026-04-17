e6fda2165e3870b6097178d9236e54c76af770b2
Toby-top <damudongni40@gmail.com>
2026-04-17 11:06:09 +0800

    Add random integer generation to the RandomNumber array

--- IGCSE_task/randomNumberGenAndSort.py
+++ IGCSE_task/randomNumberGenAndSort.py
@@ -3,3 +3,8 @@ import random
 # declare array
 RandomNumber = [0] * 100000
 CountedNumber = [[0] for _ in range(100000)]
+
+# generate random integers
+for i in range(100000):
+    RandomNumber[i] = random.randint(1, 10)
+

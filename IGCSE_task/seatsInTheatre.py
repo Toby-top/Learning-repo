0583b4ee1cbde88f14fd06520c9b53b9a0365e1c
Toby-top <damudongni40@gmail.com>
2026-04-23 09:01:47 +0800

    Implement seat booking count and output in seatsInTheatre.py

--- IGCSE_task/seatsInTheatre.py
+++ IGCSE_task/seatsInTheatre.py
@@ -1,4 +1,13 @@
 Evening = [[False for _ in range(20)] for _ in range(10)]
+Evening[0] = True
 # The above code do not need to be written during the exam
 
-#
+# Count and output the number of booked seats
+Booked = 0
+for i in range(10):
+    for j in range(20):
+        if Evening[i][j] == True:
+            Booked += 1
+
+print("Booked seats:", Booked)
+

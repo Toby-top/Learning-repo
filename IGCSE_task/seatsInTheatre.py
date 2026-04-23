b13e51f6c2a2886667d07c0b3fe8c1f7d0c16f3b
Toby-top <damudongni40@gmail.com>
2026-04-23 09:19:31 +0800

    Refactor seat booking logic and improve input validation in seatsInTheatre.py

--- IGCSE_task/seatsInTheatre.py
+++ IGCSE_task/seatsInTheatre.py
@@ -1,13 +1,24 @@
 Evening = [[False for _ in range(20)] for _ in range(10)]
-Evening[0] = True
+for i in range(20):
+    Evening[0][i] = True
+    Evening[1][0],Evening[1][1],Evening[1][2] = True
 # The above code do not need to be written during the exam
 
 # Count and output the number of booked seats
 Booked = 0
-for i in range(10):
-    for j in range(20):
-        if Evening[i][j] == True:
-            Booked += 1
+for True in Evening:
+    Booked += 1
 
 print("Booked seats:", Booked)
 
+# Allow users to input seats required
+Require = int(input("Enter the number of seats you need:"))
+
+# Validate the input
+Remaining = 20*10 - Booked
+
+while Require < 0:
+    Require = int(input("Please enter the number of seats required(>=0):"))
+if Require < Remaining:
+    print("Sorry! House full")
+    print("Number of seats left:", 20*10 - Booked)
